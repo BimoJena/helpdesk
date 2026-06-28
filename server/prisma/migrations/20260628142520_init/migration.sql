445743ed2f9efbaa7881e8bb36534197f3037c6e
BimoJena <taskbimo@gmail.com>
2026-06-28 21:15:22 +0530

    Add prisma

---
+++ server/prisma/migrations/20260628142520_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "test" (
+    "id" SERIAL NOT NULL,
+    "testName" TEXT,
+
+    CONSTRAINT "test_pkey" PRIMARY KEY ("id")
+);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { prisma } from '../lib/prisma.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +11,18 @@ app.use(express.json());
 app.get("/health", (_req, res) => {
   res.json({ status: "ok" });
 });
+
+app.post('/create-test-name', async (req, res) => {
+  const user = await prisma.test.create({
+    data: {
+      testName: req.body.testName
+    }
+  })
+  return res.status(201).json({
+    message: "user created",
+    data: user
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
